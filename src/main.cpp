95f40100984d98142ffbd9387d240686f4c6d1e6
Okabe Minato <166805519+hou-rai3@users.noreply.github.com>
2025-02-26 20:48:04 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,48 @@
 #include "PID.hpp"
 #include "firstpenguin.hpp"
 #include "mbed.h"
+#include "serial_read.hpp"
 #include <array>
 #include <cstring>
+#include <sstream>
 #include <string>
+#include <vector>
 
+PwmOut MINIMA(PA_5); // D6
 CAN can1(PA_11, PA_12, 1e6);
 CAN can2(PB_12, PB_13, 1e6);
-// FirstPenguin penguin(40, can2);
-FirstPenguin penguin(35, can2);
-PID pid_controller(0.4, 0.5, 0.001, 0.02);
-// PIDコントローラ
-PID p_move0(0.2, 0.0, 0.01, 0.02);
-PID p_move1(0.2, 0.0, 0.01, 0.02);
-PID p_move2(0.2, 0.0, 0.01, 0.02);
-PID p_move3(0.2, 0.0, 0.01, 0.02);
-
-int maxspeed = 10000;
+FirstPenguin penguin(35, can1);
 BufferedSerial pc(USBTX, USBRX, 250000);
+PID pid_controller(0.4, 0.5, 0.001, 0.02);
+PID p_move0(0.5, 0.0, 0.01, 0.02);
+PID p_move1(0.5, 0.0, 0.01, 0.02);
+PID p_move2(0.5, 0.0, 0.01, 0.02);
+PID p_move3(0.5, 0.0, 0.01, 0.02);
 uint8_t servo[8] = {};
 uint8_t DATA_move[8] = {};
 uint8_t DATA[8] = {};
-int leftJoystickX = 0, leftJoystickY = 0, rightJoystickX = 0;
-int latemode = 0, Takamatsu = 0, UnderUp = 0;
-bool triangle = false;
-int sabo = 0, sabo2 = 0;
-bool sabohoukou = false;
-int l1_direction = 0, R1_direction = 0, square_direction = 0,
-    triangle_direction = 0, circle_direction = 0;
-int bashibashi = 0;
-int guwa = 0;
 int16_t move0_speed = 0, move1_speed = 0, move2_speed = 0, move3_speed = 0;
-int servovo = 150;
+int Takamatsu = 0, UnderUp = 0, bashibashi = 0;
+int servovo = 130;
+int Kodai = 0;
+int maxspeed = 10000;
+int Kodaiho = 1000;
+auto pre_PC_1 = HighResClock::time_point();
+auto pre_PC_2 = HighResClock::time_point();
+
+DigitalIn Userbutton(BUTTON1);
+DigitalIn SW_10(PC_10);
+bool PC_10_flag = false;
+DigitalIn SW_1(PC_1);
+bool PC_1_flag = false;
+DigitalIn SW_2(PC_2);
+bool PC_2_flag = false;
 
 typedef struct {
-    signed char LX;
-    signed char LY;
-    signed char RX;
-    signed char RY;
+    double LX;
+    double LY;
+    double RX;
+    double RY;
 
     unsigned char Circle : 1;
     unsigned char Cross : 1;
@@ -61,88 +66,81 @@ typedef struct {
 
 PS2Con ps4;
 
-void controller_read(const char *buffer) {
-    if (strncmp(buffer, "n:", 2) == 0) {
-        int lx, ly, rx, ry;
-        if (sscanf(buffer, "n:%d:%d:%d:%d|", &lx, &ly, &rx, &ry) == 4) {
-            ps4.LX = static_cast<signed char>(lx);
-            ps4.LY = static_cast<signed char>(ly);
-            ps4.RX = static_cast<signed char>(rx);
-            ps4.RY = static_cast<signed char>(ry);
+std::vector<double> to_numbers(const std::string &input) {
+    std::vector<double> numbers;
+    std::stringstream ss(input);
+    std::string token;
 
-            printf("LX: %d, LY: %d, RX: %d, RY: %d\n", ps4.LX, ps4.LY, ps4.RX,
-                   ps4.RY);
-        } else {
-            printf("Error parsing joystick data: %s\n", buffer);
+    while (std::getline(ss, token, ':')) { // ':'で区切る
+        if (token.back() == '|') {         // 最後の '|' を削除
+            token.pop_back();
         }
+        numbers.push_back(std::stod(token)); // 文字列をdoubleに変換
     }
-    if (strstr(buffer, "Circle:pressing |"))
-        ps4.Circle = 1;
-    else if (strstr(buffer, "Circle:no_pressing|"))
-        ps4.Circle = 0;
-
-    if (strstr(buffer, "Cross:pressing|"))
-        ps4.Cross = 1;
-    else if (strstr(buffer, "Cross:no_pressing|"))
-        ps4.Cross = 0;
-
-    if (strstr(buffer, "Square:pressing|"))
-        ps4.Square = 1;
-    else if (strstr(buffer, "Square no_pressing|"))
-        ps4.Square = 0;
-
-    if (strstr(buffer, "Triangle:pressing|"))
-        ps4.Triangle = 1;
-    else if (strstr(buffer, "Triangle:no_pressing|"))
-        ps4.Triangle = 0;
-
-    if (strstr(buffer, "L1:pressing|"))
-        ps4.L1 = 1;
-    else if (strstr(buffer, "L1:no_pressing|"))
-        ps4.L1 = 0;
-
-    if (strstr(buffer, "R1:pressing|"))
-        ps4.R1 = 1;
-    else if (strstr(buffer, "R1:no_pressing|"))
-        ps4.R1 = 0;
-
-    if (strstr(buffer, "L2:pressing|"))
-        ps4.L2 = 1;
-    else if (strstr(buffer, "L2:no_pressing|"))
-        ps4.L2 = 0;
-
-    if (strstr(buffer, "R2:pressing|"))
-        ps4.R2 = 1;
-    else if (strstr(buffer, "R2:no_pressing|"))
-        ps4.R2 = 0;
-
-    if (strstr(buffer, "SHARE:pressing|"))
-        ps4.SHARE = 1;
-    else if (strstr(buffer, "SHARE:no_pressing|"))
-        ps4.SHARE = 0;
-
-    if (strstr(buffer, "OPTION:pressing|"))
-        ps4.OPTION = 1;
-    else if (strstr(buffer, "OPTION:no_pressing|"))
-        ps4.OPTION = 0;
-
-    if (strstr(buffer, "PS:pressing|"))
-        ps4.PS = 1;
-    else if (strstr(buffer, "PS:no_pressing|"))
-        ps4.PS = 0;
+    return numbers;
 }
 
-void serial_read() {
-    char buffer[128] = {};
-    while (1) {
-        int length = pc.read(buffer, sizeof(buffer) - 1);
-        if (length > 0) {
-            buffer[length] = '\0'; // Null 終端
-            printf("Received: %s\n", buffer);
-            controller_read(buffer);
-        }
-        ThisThread::sleep_for(10ms);
-    }
+void controller_read(const std::string buffer) {
+    if (buffer == "circle:pressing")
+        ps4.Circle = 1;
+    else if (buffer == "circle:no_pressing")
+        ps4.Circle = 0;
+    if (buffer == "cross:pressing")
+        ps4.Cross = 1;
+    else if (buffer == "cross:no_pressing")
+        ps4.Cross = 0;
+    if (buffer == "square:pressing")
+        ps4.Square = 1;
+    else if (buffer == "square:no_pressing")
+        ps4.Square = 0;
+    if (buffer == "triangle:pressing")
+        ps4.Triangle = 1;
+    else if (buffer == "triangle:no_pressing")
+        ps4.Triangle = 0;
+    if (buffer == "L1:pressing")
+        ps4.L1 = 1;
+    else if (buffer == "L1:no_pressing")
+        ps4.L1 = 0;
+    if (buffer == "R1:pressing")
+        ps4.R1 = 1;
+    else if (buffer == "R1:no_pressing")
+        ps4.R1 = 0;
+    if (buffer == "L2:pressing")
+        ps4.L2 = 1;
+    else if (buffer == "L2:no_pressing")
+        ps4.L2 = 0;
+    if (buffer == "R2:pressing")
+        ps4.R2 = 1;
+    else if (buffer == "R2:no_pressing")
+        ps4.R2 = 0;
+    if (buffer == "SHARE:pressing")
+        ps4.SHARE = 1;
+    else if (buffer == "SHARE:no_pressing")
+        ps4.SHARE = 0;
+    if (buffer == "OPTION:pressing")
+        ps4.OPTION = 1;
+    else if (buffer == "OPTION:no_pressing")
+        ps4.OPTION = 0;
+    if (buffer == "PS:pressing")
+        ps4.PS = 1;
+    else if (buffer == "PS:no_pressing")
+        ps4.PS = 0;
+    if (buffer == "up:pressing")
+        ps4.Up = 1;
+    else if (buffer == "up:no_pressing")
+        ps4.Up = 0;
+    if (buffer == "dowm:pressing")
+        ps4.Down = 1;
+    else if (buffer == "dowm:no_pressing")
+        ps4.Down = 0;
+    if (buffer == "left:pressing")
+        ps4.Left = 1;
+    else if (buffer == "left:no_pressing")
+        ps4.Left = 0;
+    if (buffer == "right:pressing")
+        ps4.Right = 1;
+    else if (buffer == "right:no_pressing")
+        ps4.Right = 0;
 }
 
 void CANReceive() {
@@ -171,76 +169,125 @@ void CANReceive() {
     }
 }
 
-bool Square_flag = false;
-bool Square_state = 0;
-
 void CANSend() {
     while (1) {
+        PC_10_flag = SW_10.read();
+        PC_1_flag = SW_1.read();
+        PC_2_flag = SW_2.read();
+
         // バシバシ前後
-        if (ps4.R2 == 1) {
-            penguin.pwm[0] = -8000;
-            penguin.pwm[1] = 8000;
+        if (ps4.R1 == 1) {
+            penguin.pwm[0] = 10000;
+            penguin.pwm[1] = -10000;
         }
-        if (ps4.R2 == 0 && ps4.L2 == 0) {
+        if (ps4.L1 == 1) {
+            penguin.pwm[0] = -10000;
+            penguin.pwm[1] = 10000;
+        }
+
+        if (ps4.L1 == 0 && ps4.R1 == 0) {
             penguin.pwm[0] = 0;
             penguin.pwm[1] = 0;
         }
-        if (ps4.L2 == 1) {
-            penguin.pwm[0] = -8000;
-            penguin.pwm[1] = 8000;
+        if (PC_1_flag == 0) {
+            auto now_PC_1 = HighResClock::now();
+            if (pre_PC_1 == HighResClock::time_point()) {
+                pre_PC_1 = now_PC_1;
+            }
+            if (now_PC_1 - pre_PC_1 <= 1000ms) {
+                penguin.pwm[0] = 0;
+            }
+        } else {
+            pre_PC_1 = HighResClock::time_point();
+        }
+
+        if (PC_2_flag == 0) {
+            auto now_PC_2 = HighResClock::now();
+            if (pre_PC_2 == HighResClock::time_point()) {
+                pre_PC_2 = now_PC_2;
+            }
+            if (now_PC_2 - pre_PC_2 <= 1000ms) {
+                penguin.pwm[1] = 0;
+            }
+        } else {
+            pre_PC_2 = HighResClock::time_point();
         }
         // 床
-        if (ps4.Square == 1) {
-            if (Square_flag) {
-                UnderUp = 4000;
-            } else {
-                UnderUp = -2000;
-            }
-        } else if (ps4.Square == 0) {
-            UnderUp = 0;
+        if (ps4.R2 == 1) {
+            int16_t UnderUp416 = static_cast<int16_t>(1500);
+            DATA[6] = UnderUp416 >> 8;
+            DATA[7] = UnderUp416 & 0xFF;
         }
-        if (ps4.Square == 1 && Square_state == 0) {
-            Square_flag = !Square_flag;
+        if (ps4.R2 == 0 && ps4.L2 == 0) {
+            int16_t UnderUp416 = static_cast<int16_t>(0);
+            DATA[6] = UnderUp416 >> 8;
+            DATA[7] = UnderUp416 & 0xFF;
         }
-        Square_state = ps4.Square;
+        if (ps4.L2 == 1) {
+            printf("get down\n");
+            int16_t UnderUp416 = static_cast<int16_t>(1500);
+            DATA[6] = -UnderUp416 >> 8;
+            DATA[7] = -UnderUp416 & 0xFF;
+        }
+
         // 高松
-        if (ps4.R1 == 1) {
-            Takamatsu = 10000;
-        }
-        if (ps4.R1 == 0 && ps4.L1 == 0) {
-            Takamatsu = 0;
-        }
-        if (ps4.L1 == 1) {
-            Takamatsu = -10000;
+        if (ps4.Up == 1 && PC_10_flag == 1) {
+            int16_t Takamatsu416 = static_cast<int16_t>(3000);
+            DATA[0] = Takamatsu416 >> 8;
+            DATA[1] = Takamatsu416 & 0xFF;
+        } else if (ps4.Up == 1 && PC_10_flag == 0) {
+            int16_t Takamatsu416 = static_cast<int16_t>(500);
+            DATA[0] = Takamatsu416 >> 8;
+            DATA[1] = Takamatsu416 & 0xFF;
+        } else if (ps4.Up == 0 && ps4.SHARE == 0) {
+            int16_t Takamatsu416 = static_cast<int16_t>(0);
+            DATA[0] = Takamatsu416 >> 8;
+            DATA[1] = Takamatsu416 & 0xFF;
+        } else if (ps4.SHARE == 1) {
+            int16_t Takamatsu416 = static_cast<int16_t>(1500);
+            DATA[0] = -Takamatsu416 >> 8;
+            DATA[1] = -Takamatsu416 & 0xFF;
         }
         // 発射
         if (ps4.Triangle == 1) {
-            penguin.pwm[2] = std::min(5000, penguin.pwm[2] + 300);
-            penguin.pwm[3] = std::max(-5000, penguin.pwm[3] - 300);
+            penguin.pwm[2] = std::min(5000, penguin.pwm[2] + 600);
+            penguin.pwm[3] = std::max(-5000, penguin.pwm[3] - 600);
         } else if (ps4.Triangle == 0) {
-            penguin.pwm[2] = std::max(0, penguin.pwm[2] - 300);
-            penguin.pwm[3] = std::min(0, penguin.pwm[3] + 300);
+            penguin.pwm[2] = std::max(0, penguin.pwm[2] - 600);
+            penguin.pwm[3] = std::min(0, penguin.pwm[3] + 600);
         }
         // バシバシ
         if (ps4.Circle == 1) {
-            bashibashi = 5000;
-        }
-        if (ps4.Circle == 0 && ps4.Circle == 0) {
+            bashibashi = 7000;
+        } else if (ps4.Circle == 0) {
             bashibashi = 0;
         }
-        if (ps4.Circle == 1) {
-            bashibashi = -5000;
-        }
+
         // サーボ
-        else if (ps4.Cross == 1) {
-            servovo = (servovo == 0) ? 150 : (servovo == 150 ? 90 : 0);
+        if (ps4.Cross == 1) {
+            servovo = 0;
+        } else if (ps4.Square == 1) {
+            servovo = 150;
         }
 
+        if (ps4.OPTION == 1) {
+            Kodaiho = std::min(1600, Kodaiho + 8);
+            MINIMA.pulsewidth_us(Kodaiho);
+        } else if (ps4.OPTION == 0) {
+            Kodaiho = std::max(1000, Kodaiho - 8);
+            MINIMA.pulsewidth_us(Kodaiho);
+        }
+        if (ps4.Right == 1) {
+            Kodai = 40;
+        }
+        if (ps4.Left == 1) {
+            Kodai = 0;
+        }
         // 移動
-        int move0 = ps4.LX * 0.6 - ps4.LY * 0.6 + ps4.RX * 0.8;
-        int move1 = -ps4.LX * 0.6 - ps4.LY * 0.6 + ps4.RX * 0.8;
-        int move2 = -ps4.LX * 0.6 + ps4.LY * 0.6 + ps4.RX * 0.8;
-        int move3 = ps4.LY * 0.6 + ps4.LX * 0.6 + ps4.RX * 0.8;
+        int move0 = (ps4.LX - ps4.LY - ps4.RX * 0.8) * 10000;
+        int move1 = -(ps4.LX - ps4.LY + ps4.RX * 0.8) * 10000;
+        int move2 = -(ps4.LX + ps4.LY + ps4.RX * 0.8) * 10000;
+        int move3 = (ps4.LY + ps4.LX - ps4.RX * 0.8) * 10000;
         move0 = std::min(move0, maxspeed);
         move1 = std::min(move1, maxspeed);
         move2 = std::min(move2, maxspeed);
@@ -258,41 +305,43 @@ void CANSend() {
         int move2_pid = p_move2.calculate(move2, move2_speed);
         int move3_pid = p_move3.calculate(move3, move3_speed);
 
-        printf("PID Output: move0_pid = %d, move1_pid = %d, move2_pid "
-               "= %d, "
-               "move3_pid = %d\n",
-               move0_pid, move1_pid, move2_pid, move3_pid);
+        // printf("PID Output: move0_pid = %d, move1_pid = %d, move2_pid "
+        //        "= %d, "
+        //        "move3_pid = %d\n",
+        //        move0_pid, move1_pid, move2_pid, move3_pid);
 
         // 足回り
         int16_t move0416 = static_cast<int16_t>(move0_pid);
         int16_t move1416 = static_cast<int16_t>(move1_pid);
         int16_t move2416 = static_cast<int16_t>(move2_pid);
         int16_t move3416 = static_cast<int16_t>(move3_pid);
-        DATA_move[0] = move0416 >> 8;   // MSB
-        DATA_move[1] = move0416 & 0xFF; // LSB
+        DATA_move[0] = move0416 >> 8;
+        DATA_move[1] = move0416 & 0xFF;
 
-        DATA_move[2] = move1416 >> 8;   // MSB
-        DATA_move[3] = move1416 & 0xFF; // LSB
+        DATA_move[2] = move1416 >> 8;
+        DATA_move[3] = move1416 & 0xFF;
 
-        DATA_move[4] = move2416 >> 8;   // MSB
-        DATA_move[5] = move2416 & 0xFF; // LSB
+        DATA_move[4] = move2416 >> 8;
+        DATA_move[5] = move2416 & 0xFF;
 
-        DATA_move[6] = move3416 >> 8;   // MSB
-        DATA_move[7] = move3416 & 0xFF; // LSB
+        DATA_move[6] = move3416 >> 8;
+        DATA_move[7] = move3416 & 0xFF;
+
         // 機構
-        int16_t Takamatsu416 = static_cast<int16_t>(Takamatsu);
-        int16_t UnderUp416 = static_cast<int16_t>(UnderUp);
         int16_t bashibashiInt16 = static_cast<int16_t>(bashibashi);
-        DATA[0] = Takamatsu416 >> 8;      // MSB
-        DATA[1] = Takamatsu416 & 0xFF;    // LSB
-        DATA[2] = bashibashiInt16 >> 8;   // MSB
-        DATA[3] = bashibashiInt16 & 0xFF; // LSB
 
-        DATA[4] = 0 >> 8;            // MSB
-        DATA[5] = 0 & 0xFF;          // LSB
-        DATA[6] = UnderUp416 >> 8;   // MSB
-        DATA[7] = UnderUp416 & 0xFF; // LSB
+        DATA[2] = bashibashiInt16 >> 8;
+        DATA[3] = bashibashiInt16 & 0xFF;
+        DATA[4] = 0 >> 8;
+        DATA[5] = 0 & 0xFF;
 
+        servo[0] = Kodai;
+        servo[1] = 0;
+        servo[2] = 0;
+        servo[3] = 0;
+        servo[4] = 0;
+        servo[5] = 0;
+        servo[6] = 0;
         servo[7] = servovo;
 
         CANMessage msg_move(0x200, DATA_move, 8);
@@ -325,23 +374,38 @@ void CANSend() {
 }
 
 int main() {
-    printf("start\n");
-
+    std::vector<double> joy_nums;
     pc.set_baud(115200);
     pc.set_blocking(false);
-
+    servo[0] = Kodai;
     servo[7] = servovo;
     CANMessage servo_msg(140, servo, 8);
     can2.write(servo_msg);
-
-    Thread thread1; // コントローラーの状態を受け取る
-    thread1.start(serial_read);
-    Thread thread2; // 足回りのセンサ情報を受け取る
+    serial_unit serial(pc);
+    Thread thread2;
+    Thread thread3;
     thread2.start(CANReceive);
-    Thread thread3; // 機械制御とmsgを送る
     thread3.start(CANSend);
 
-    while (1) {
-        ThisThread::sleep_for(10ms);
+    printf("[controller_tester]setup!!\n");
+
+    SW_10.mode(PullUp);
+    SW_1.mode(PullUp);
+    SW_2.mode(PullUp);
+
+    while (true) {
+        std::string msg = serial.read_serial();
+        if (msg != "") {
+            if (msg[0] == 'n') {
+                msg.erase(0, 2);
+                joy_nums = to_numbers(msg);
+                ps4.LX = joy_nums[0];
+                ps4.LY = joy_nums[1];
+                ps4.RX = joy_nums[2];
+                ps4.RY = joy_nums[3];
+            } else {
+                controller_read(msg);
+            }
+        }
     }
 }
